b60ada60b2f99f3065e886e4b0027944e0c909b6
Brian Chapman <chapman.brian86@gmail.com>
2023-05-30 16:06:29 -0400

    Initial setup for CRUD operations complete. More styling and navigation needed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do |x| 
+    Post.create(title: "Blog Post #{x}", body: "Lorem ipsum, you my only friend.")
+end
