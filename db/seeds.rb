05a5bbbe61fa1ad2b08fa09dc1a8cb516807f4d9
Brian Chapman <chapman.brian86@gmail.com>
2023-06-05 02:48:53 -0400

    Added logic for edited vs initial posting

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.create(email: "brian@example.com", password: "Password", password_confirmation: "Password")
+User.create(email: "brian@example.com", name:"Brian", password: "Password", password_confirmation: "Password")
+User.create(email: "mae@example.com", name:"Mae", password: "Password", password_confirmation: "Password")
 
 10.times do |x| 
     Post.create(title: "Blog Post #{x}", body: "Lorem ipsum, you my only friend.", user_id: User.first.id)
